f84d1c73f552c7f16ea7c1c96750d7ddce8d1f29
solenne2404 <solenne2404@gmail.com>
2019-11-18 23:06:42 +0100

    Requiring Users To Login

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'So'}
     posts = [
@@ -33,20 +35,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    
-    form = LoginForm()
-    if form.validate_on_submit():
-        
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
