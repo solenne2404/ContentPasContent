8674557a1e09317f060f49661dfb69dbd5056b09
solenne2404 <solenne2404@gmail.com>
2019-11-18 22:53:04 +0100

    login users in

--- Flask/app/models.py
+++ Flask/app/models.py
@@ -1,9 +1,11 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -20,6 +22,26 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
 
+class Promo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Promo {}>'.format(self.name)
+
+
+class FK_User_Promo(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    promo_id = db.Column(db.Integer, db.ForeignKey('promo.id'), primary_key=True)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+
 # class Post(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     body = db.Column(db.String(140))
@@ -28,15 +50,3 @@ class User(db.Model):
 
 #     def __repr__(self):
 #         return '<Post {}>'.format(self.body)
-
-class Promo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True, unique=True)
-
-    def __repr__(self):
-        return '<Promo {}>'.format(self.name)
-
-class FK_User_Promo(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    promo_id = db.Column(db.Integer, db.ForeignKey('promo.id'), primary_key=True)
-
