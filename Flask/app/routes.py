1c092e319e20fc02e0b77881175c9219faac4d04
solenne2404 <solenne2404@gmail.com>
2019-11-18 14:21:10 +0100

    first template

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -1,6 +1,10 @@
 from app import app
 
+from flask import render_template
+from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
