938e5f260113c73e653e295bb205618346f33d4b
palplos <Palplos@Palplos.com>
2019-11-22 10:38:23 +0100

    Add the week number and order by class

--- Bastien/Actualisation_Donnee.py
+++ Bastien/Actualisation_Donnee.py
@@ -21,6 +21,56 @@ sheet_clean_display = client.open("Formulaire").worksheet("Affichage")
 sheet_clean_quartile = client.open("Formulaire").worksheet("Quartile/moyenne")
 #All clean sheet in one list
 sheet_table = [sheet_clean_CDA_1, sheet_clean_CDA_2, sheet_clean_DWWM_1, sheet_clean_DWWM_2, sheet_clean_average, sheet_clean_end, sheet_clean_display]
+#All class's name
+class_name = ["CDA 1","CDA 2","DWWM 1","DWWM 2"]
+#All useable's key
+line_name = ["Nom","Prénom","Q1","Q2","Q3","Q4","Q5","Q6","Date"]
+
+#List for having the average for all class'
+average_qu3 = []
+average_qu4 = []
+average_qu5 = []
+average_qu6 = []
+
+#line/column for sheet_clean_number
+column = [2,12,2,12]
+line_number = [2,2,10,10]
+col_quantile = [2,4,6,2,4,6,2,4,6,2,4,6,2,4,6,2,4,6,9,11,13,9,11,13,9,11,13,9,11,13,9,11,13,9,11,13 ]
+line_quantile = [2,4,6,8,10,12,2,4,6,8,10,12,15,17,19,21,23,25,15,17,19,21,23,25]
+
+
+#variable for deleting row in sheet_base
+end_sheet = 1
+
+#use for change the class name
+class_change = 0
+
+#change average number for all class'
+data_average_list = 0
+
+#use for change column in sheet_clean_end
+column_end = 2
+
+#change line/column for all class' average
+line_average = 2
+column_average = 2
+
+#change the return of line/column list
+change_display = 0
+
+#while loop
+average_change = 1
+
+#Import all answers
+value = sheet_base.get_all_records()
+
+nbr_col_quant = 0
+nbr_line_quant = 0
+
+table = {}
+
+for line in value:
+
     if line["Formation actuelle"] not in table:
         table[line["Formation actuelle"]] = []
 
@@ -35,3 +85,39 @@ sheet_table = [sheet_clean_CDA_1, sheet_clean_CDA_2, sheet_clean_DWWM_1, sheet_c
         "Q6": int(line["Comment évaluez-vous la satisfaction de vos attentes personnelles ?"]),
         "Date": line["Horodateur"]
     })
+
+while class_change < len(class_name):
+    #check line for writing data
+    line_check = 2
+    #list for puting some answer (number of 1, number of 2...) for each question
+    Q1 = [0, 0, 0, 0, 0]
+    Q2 = [0, 0, 0, 0, 0]
+    Q3 = [0, 0, 0, 0, 0]
+    Q4 = [0, 0, 0, 0, 0]
+    Q5 = [0, 0, 0, 0, 0]
+    Q6 = [0, 0, 0, 0, 0]
+
+    for line in table[class_name[class_change]]:
+        #while loop
+        y = 0
+
+        #defined column for writing
+        column_writing = 2
+        while y < 8 :
+            if not sheet_table[class_change].row_values(line_check):
+                date_hours = line[line_name[8]].split(" ")
+                just_date = date_hours[0].split("/")
+                day,month,year = int(just_date[0]), int(just_date[1]), int(just_date[2])
+                date_conversion = date_test.date(year,month,day)
+                week_number = date_test.date.isocalendar(date_conversion)
+                sheet_table[class_change].update_cell(line_check,1, week_number[1])
+                sleep(1)
+                while y < 8 :
+                    sheet_table[class_change].update_cell(line_check,column_writing, line[line_name[y]])
+                    sleep(1)
+                    y += 1
+                    column_writing += 1
+                line_check += 1
+            else :
+                line_check += 1
+    class_change += 1
