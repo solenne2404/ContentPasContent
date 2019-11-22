f46e142e8e2b8bc4c54939269b8c9b0c66d58baa
palplos <Palplos@Palplos.com>
2019-11-22 10:34:11 +0100

    Import all the answer

--- Bastien/Actualisation_Donnee.py
+++ Bastien/Actualisation_Donnee.py
@@ -21,3 +21,17 @@ sheet_clean_display = client.open("Formulaire").worksheet("Affichage")
 sheet_clean_quartile = client.open("Formulaire").worksheet("Quartile/moyenne")
 #All clean sheet in one list
 sheet_table = [sheet_clean_CDA_1, sheet_clean_CDA_2, sheet_clean_DWWM_1, sheet_clean_DWWM_2, sheet_clean_average, sheet_clean_end, sheet_clean_display]
+    if line["Formation actuelle"] not in table:
+        table[line["Formation actuelle"]] = []
+
+    table[line["Formation actuelle"]].append({
+        "Nom": line["Nom (Entièrement en majuscule)"],
+        "Prénom": line["Prénom (Entièrement en minuscule)"],
+        "Q1": int(line["Comment évaluez-vous les méthodes pédagogique et d'animation proposées cette semaine ?"]),
+        "Q2": int(line["Comment évaluez-vous votre progression pendant cette semaine ?"]),
+        "Q3": int(line["Comment évaluez-vous l'organisation matérielle de la formation (rythme, planning, horaire) ?"]),
+        "Q4": int(line["Comment évaluez-vous les moyens pédagogiques mis à disposition (supports, documentation, ...) ?"]),
+        "Q5": int(line["Comment évaluez-vous les échanges dans votre groupe ?"]),
+        "Q6": int(line["Comment évaluez-vous la satisfaction de vos attentes personnelles ?"]),
+        "Date": line["Horodateur"]
+    })
